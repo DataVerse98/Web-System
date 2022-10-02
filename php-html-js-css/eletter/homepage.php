5ae8129362269db515711bdb11b3b873bdf38daa
DataVerse98 <114841564+DataVerse98@users.noreply.github.com>
2022-10-02 10:54:31 +0800

    Add files via upload

---
+++ php-html-js-css/eletter/homepage.php
@@ -0,0 +1,60 @@
+<html>
+	<head>
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="all">
+		<link rel="icon" type="image/png" href="images/logo.png" />
+		
+		<title>LGSystem</title>
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+			<span class="navbar-brand">
+				<img src="./images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            LGSystem
+			</span>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<!--<li class="nav-item">
+						<a class="nav-link" href="index.php">Generate</a>
+					</li>-->
+					<!--<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Generate</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="index.php" value="receive_date">Manual</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="newIndex2.php" value="">Import</a>
+						</div>
+					</li>-->
+					<!--<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Order</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Ascending</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Descending</a>
+						</div>
+					</li>-->
+					<li class="nav-item active">
+						<a class="nav-link" href="login.php">Log Out</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		
+		<div align ="center" style="margin-top:5%">
+			<img class="mb-4" src="./images/logo-big.png" alt="" width="240" height="135">
+			<h2>LGSystem UiTM Sarawak</h2>
+			<br>
+			<a class="btn btn-primary" href="index.php">Manual</a>
+			<a class="btn btn-primary" href="newIndex2.php">Import</a>
+			<p class="mt-5 text-muted">Developed by Abdul Azim Bin Ismail <br> Penyelidikan Jaringan Industri UiTM Cawangan Sarawak <br> &copy; All Rights Reserved 2020</p>
+			
+		</div>
+		
+		
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="bootstrap/js/bootstrap.min.js" ></script>
+		
+	</body>
+</html>
---
+++ php-html-js-css/eletter/surat_pelanjutan_tempoh.php
@@ -0,0 +1,200 @@
+<?php 
+	include "conn.php";
+	
+	$id = $_GET['id'];
+	$sql = "SELECT * FROM certificate WHERE rec_id = $id";
+	$query = $conn -> query($sql);
+	$row = $query -> fetch_assoc();
+		
+		$ref_no = $row['ref_no'];
+		$kp = $row['kp_id'];		
+		$ap1 = $row['ap1_id'];
+		$ap2 = $row['ap2_id'];
+		$ap3 = $row['ap3_id'];
+		$ap4 = $row['ap4_id'];
+		$tajuk = $row['title'];
+		$info = $row['info'];
+		$tempoh = $row['tempoh'];
+		$tarikh_mula = strtotime($row['tarikh_mula']);
+		$tarikh_tamat = strtotime($row['tarikh_tamat']);
+		$generate = strtotime($row['date_generate']);
+		
+		$dayM = date('d', $tarikh_mula);
+		$monthM = date('m', $tarikh_mula);
+		$mm = array("Januari", "Februari","Mac", "April", "Mei", "Jun", "Julai", "Ogos", "September", "October", "November","Disember");
+		$monthM = $mm[$monthM-1];
+		$yearM = date('Y', $tarikh_mula);
+		
+		$dayT = date('d', $tarikh_tamat);
+		$monthT = date('m', $tarikh_tamat);
+		$mt = array("Januari", "Februari","Mac", "April", "Mei", "Jun", "Julai", "Ogos", "September", "October", "November","Disember");
+		$monthT = $mt[$monthT-1];
+		$yearT = date('Y', $tarikh_tamat);
+	
+?>
+<html>
+	<head>
+		<style>
+		html, body {
+		  background: rgb(204,204,204); 
+		  font: 10pt "Arial";
+		  line-height: 1.4em;
+		  color : 111;
+		}
+		body .page {
+		  background: white;
+		  display: block;
+		  margin: 0 auto;
+		  margin-bottom: 0.5cm;
+		  box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
+		  width: 22cm;
+		  height: 28.5cm;
+		}
+
+		.print {
+			background: transparent;
+			width: 21cm;
+			height: 65.0 cm;
+			display: block;
+			margin: 0 auto;
+			
+		}
+
+		p {
+			padding-left: 80px;
+			padding-right: 80px;
+			text-align: justify;
+			//letter-spacing: 1px;
+			word-spacing: 3px;
+			color: #000000;
+		}
+
+
+		@media print {
+		  body, page[size="A4"] {
+			margin: 0;
+			box-shadow: 0;
+		  }
+		}
+
+		</style>
+		
+				
+		<script src="js/jquery.min.js"></script>
+		<script type="text/javascript" >
+			alert("To print the certificate , Please click ctrl+P");
+		</script>
+		
+				
+		<?php
+			//$date = date("j F Y");
+			$day = date('d',$generate);
+			$month = date('m',$generate);
+			$m = array("Januari", "Februari","Mac", "April", "Mei", "Jun", "Julai", "Ogos", "September", "October", "November","Disember");
+			$month = $m[$month-1];
+			$year = date('Y',$generate);
+		?>
+		<link rel="icon" type="image/png" href="images/logo.png" />
+		<title>ER1</title>
+	</head>
+		
+	<body> <!--onLoad="javascript:print()"-->
+		<?php if($ap3==null){?>
+		<div class="page">
+			<img src="images/header_uitm.png" style="margin-left: 30px; margin-right:30px; margin-top:25px; width: 20.5cm" alt="Header img"> <!--opacity: 0-->
+			<p style="padding-left: 50%"> Surat Kami : <b><?php echo "$ref_no"?></b></p>
+			<p style="padding-left: 50%"> Tarikh <span style="display: inline-block; margin-left: 36px;"> :</span> <b><?php echo "$day $month $year"?></b></p>
+			<br>
+			<?php if($kp!=null){ ?>
+			<p><b><?php include "ketua_projek.php"; echo "$name <br> Ketua Projek <br> Universiti Teknologi MARA Cawangan $cawangan <br> Kampus Samarahan";?></b></p>
+			<?php }?>
+			<?php if($ap1!=null){?>
+			<p><b><?php include "ahli_projek.php"; echo "$nameap1 <br> Ahli Projek <br> Universiti Teknologi MARA Cawangan $cawanganap1 <br> Kampus Samarahan"; ?></b></p>
+			<?php }?>
+			<?php if($ap2!=null){?>
+			<p><b><?php  include "ahli_projek2.php"; echo "$nameap2 <br> Ahli Projek <br> Universiti Teknologi MARA Cawangan $cawanganap2 <br> Kampus Samarahan"; ?></b></p>
+			<?php }?>
+			<p><br>Prof/Prof Madya/Dr/Tuan/Puan </p>
+			<p style="text-transform: uppercase;"><strong>KELULUSAN PELANJUTAN TEMPOH PROJEK PENYELIDIKAN KALI PERTAMA:<br>
+				DANA KECEMERLANGAN UiTM SARAWAK<br> "<?php echo"$tajuk"?>"</strong></p>
+			<p>Dengan hormatnya perkara di atas adalah dirujuk.</p>
+			
+			<p style="text-align:justify;">2.&Tab;&Tab; Sukacita dimaklumkan bahawa Jawatankuasa Penyelidikan Negeri (JPN) telah <b>meluluskan</b> permohonan pelanjutan tempoh projek penyelidikan <b>kali pertama</b> bagi tajuk projek tuan/puan tersebut di atas, dengan syarat-syarat seperti berikut:</p>
+			<p style="text-align: justify; padding-left: 120px"> i.&Tab; Tempoh pelanjutan yang telah diluluskan ialah <?php echo"$tempoh"?>, iaitu bermula <b><?php echo "$dayM $monthM $yearM"?> hingga <?php echo "$dayT $monthT $yearT"?></b>.</p>
+			<p style="text-align: justify; padding-left: 120px"> ii.&Tab; Pelanjutan tempoh projek penyelidikan kali kedua adalah <u>tidak dibenarkan</u> bagi Geran ini.  Sehubungan itu, penyelidik adalah dinasihatkan untuk menggunakan sebaik-baiknya baki tempoh yang ada untuk menamatkan projek penyelidikan ini.</p>
+			<p style="text-align: justify; padding-left: 120px"> iii.&Tab; Penggunaan peruntukan dan perbelanjaan setelah tamat projek adalah tidak dibenarkan.  Permohonan perolehan yang melibatkan Pesanan Tempatan (LO) oleh penyelidik hendaklah dilakukan tidak lewat daripada tiga (3) bulan sebelum tamat projek.</p>
+			<p style="text-align: justify; padding-left: 120px"> iv.&Tab; Pembelian peralatan ICT (komputer, peranti storan, toner pencetak, dan sebagainya) dan peralatan pejabat (alat tulis, kertas, dan sebagainya) adalah <b>tidak dibenarkan menggunakan Geran ini</b>.  Lain-lain pembelian bahan/peralatan adalah diminta agar tuan/puan mematuhi Peraturan Kewangan dan Perakaunan UiTM yang sedang berkuatkuasa.</p>
+			
+		</div>
+		<div class="page">
+			<br><br><br>
+			<p style="text-align: justify; padding-left: 120px"> v.&Tab; Bagi tujuan audit pemantauan geran penyelidikan, semua dokumen kewangan perbelanjaan peruntukan menggunakan Geran ini hendaklah dibuat satu (1) salinan ke dalam Fail Projek di Pejabat RMU.</p>
+			<p style="text-align: justify; padding-left: 120px"> vi.&Tab; Semua urusan surat-menyurat rasmi penyelidik berkaitan Projek ini hendaklah menggunakan Nombor Fail Projek mengikut nombor Surat Tawaran projek ini, dan hendaklah dibuat satu (1) salinan ke dalam Fail Projek di Pejabat RMU. </p>
+			<p style="text-align: justify; padding-left: 120px"> vii.&Tab; Surat Pengesahan Penerimaan Borang Tamat Projek dan/atau Surat Pengesahan Penerimaan Laporan Akhir Projek akan dikeluarkan kepada penyelidik setelah Borang Tamat Projek dan/atau Laporan Akhir Projek dikemukakan kepada pejabat RMU. </p>
+			<p style="text-align: justify; padding-left: 120px"> viii.&Tab; Penghargaan kepada Geran Dana Kecemerlangan UiTM Sarawak perlu dinyatakan dengan jelas dalam setiap penerbitan atau penghasilan harta intelek menggunakan Geran ini. </p>
+			<p style="text-align: justify; padding-left: 120px"> ix.&Tab; Universiti berhak membatalkan kelulusan geran dan menamatkan projek sekiranya peruntukan tidak digunakan setelah enam (6) bulan daripada tarikh kelulusan dikeluarkan atau prestasi projek tidak memuaskan. </p>
+			<p style="text-align: justify; padding-left: 120px"> x.&Tab; Syarat-syarat tersebut di atas adalah tertakluk kepada pindaan Garis Panduan Pemantauan Geran Penyelidikan UiTM dari semasa ke semasa.  Penyelidik adalah diingatkan untuk sentiasa merujuk kepada Garis Panduan tersebut. </p>
+			<p style="text-align:justify;">3.&Tab;&Tab; Kelulusan ini <?php echo "$info"?>.</p>
+			<p>Sekian, terima kasih.</p>
+			<p><strong>“SELAMAT MENJALANKAN PENYELIDIKAN DENGAN JAYANYA”</strong></p>
+			<p>Yang benar</p>
+			<br>
+			<p><strong>PROFESOR DATO DR. JAMIL HAJI HAMALI<br>Rektor</strong></p>
+		</div>
+		<?php }else{?>
+			<div class="page">
+			<img src="images/header_uitm.png" style="margin-left: 30px; margin-right:30px; margin-top:25px; width: 20.5cm" alt="Header img"> <!--opacity: 0-->
+			<p style="padding-left: 50%"> Surat Kami : <b><?php echo "$ref_no"?></b></p>
+			<p style="padding-left: 50%"> Tarikh <span style="display: inline-block; margin-left: 36px;"> :</span> <b><?php echo "$day $month $year"?></b></p>
+			<br>
+			<?php if($kp!=null){ ?>
+			<p><b><?php include "ketua_projek.php"; echo "$name <br> Ketua Projek <br> Universiti Teknologi MARA Cawangan $cawangan <br> Kampus Samarahan";?></b></p>
+			<?php }?>
+			<?php if($ap1!=null){?>
+			<p><b><?php include "ahli_projek.php"; echo "$nameap1 <br> Ahli Projek <br> Universiti Teknologi MARA Cawangan $cawanganap1 <br> Kampus Samarahan"; ?></b></p>
+			<?php }?>
+			<?php if($ap2!=null){?>
+			<p><b><?php  include "ahli_projek2.php"; echo "$nameap2 <br> Ahli Projek <br> Universiti Teknologi MARA Cawangan $cawanganap2 <br> Kampus Samarahan"; ?></b></p>
+			<?php }?>
+			<?php if($ap3!=null){?>
+			<p><b><?php  include "ahli_projek3.php"; echo "$nameap3 <br> Ahli Projek <br> Universiti Teknologi MARA Cawangan $cawanganap3 <br> Kampus Samarahan"; ?></b></p>
+			<?php }?>
+			<?php if($ap4!=null){?>
+			<p><b><?php  include "ahli_projek4.php"; echo "$nameap4 <br> Ahli Projek <br> Universiti Teknologi MARA Cawangan $cawanganap4 <br> Kampus Samarahan"; ?></b></p>
+			<?php }?>
+			<p><br>Prof/Prof Madya/Dr/Tuan/Puan </p>
+			<p style="text-transform: uppercase;"><strong>KELULUSAN PELANJUTAN TEMPOH PROJEK PENYELIDIKAN KALI PERTAMA:<br>
+				DANA KECEMERLANGAN UiTM SARAWAK<br> "<?php echo"$tajuk"?>"</strong></p>
+			<p>Dengan hormatnya perkara di atas adalah dirujuk.</p>
+			
+			<p style="text-align:justify;">2.&Tab;&Tab; Sukacita dimaklumkan bahawa Jawatankuasa Penyelidikan Negeri (JPN) telah <b>meluluskan</b> permohonan pelanjutan tempoh projek penyelidikan <b>kali pertama</b> bagi tajuk projek tuan/puan tersebut di atas, dengan syarat-syarat seperti berikut:</p>
+			<p style="text-align: justify; padding-left: 120px"> i.&Tab; Tempoh pelanjutan yang telah diluluskan ialah <?php echo"$tempoh"?>, iaitu bermula <b><?php echo "$dayM $monthM $yearM"?> hingga <?php echo "$dayT $monthT $yearT"?></b>.</p>
+			<p style="text-align: justify; padding-left: 120px"> ii.&Tab; Pelanjutan tempoh projek penyelidikan kali kedua adalah <u>tidak dibenarkan</u> bagi Geran ini.  Sehubungan itu, penyelidik adalah dinasihatkan untuk menggunakan sebaik-baiknya baki tempoh yang ada untuk menamatkan projek penyelidikan ini.</p>
+		</div>
+		<div class="page">
+			<br><br><br>
+			<p style="text-align: justify; padding-left: 120px"> iii.&Tab; Penggunaan peruntukan dan perbelanjaan setelah tamat projek adalah tidak dibenarkan.  Permohonan perolehan yang melibatkan Pesanan Tempatan (LO) oleh penyelidik hendaklah dilakukan tidak lewat daripada tiga (3) bulan sebelum tamat projek.</p>
+			<p style="text-align: justify; padding-left: 120px"> iv.&Tab; Pembelian peralatan ICT (komputer, peranti storan, toner pencetak, dan sebagainya) dan peralatan pejabat (alat tulis, kertas, dan sebagainya) adalah <b>tidak dibenarkan menggunakan Geran ini</b>.  Lain-lain pembelian bahan/peralatan adalah diminta agar tuan/puan mematuhi Peraturan Kewangan dan Perakaunan UiTM yang sedang berkuatkuasa.</p>
+			<p style="text-align: justify; padding-left: 120px"> v.&Tab; Bagi tujuan audit pemantauan geran penyelidikan, semua dokumen kewangan perbelanjaan peruntukan menggunakan Geran ini hendaklah dibuat satu (1) salinan ke dalam Fail Projek di Pejabat RMU.</p>
+			<p style="text-align: justify; padding-left: 120px"> vi.&Tab; Semua urusan surat-menyurat rasmi penyelidik berkaitan Projek ini hendaklah menggunakan Nombor Fail Projek mengikut nombor Surat Tawaran projek ini, dan hendaklah dibuat satu (1) salinan ke dalam Fail Projek di Pejabat RMU. </p>
+			<p style="text-align: justify; padding-left: 120px"> vii.&Tab; Surat Pengesahan Penerimaan Borang Tamat Projek dan/atau Surat Pengesahan Penerimaan Laporan Akhir Projek akan dikeluarkan kepada penyelidik setelah Borang Tamat Projek dan/atau Laporan Akhir Projek dikemukakan kepada pejabat RMU. </p>
+			<p style="text-align: justify; padding-left: 120px"> viii.&Tab; Penghargaan kepada Geran Dana Kecemerlangan UiTM Sarawak perlu dinyatakan dengan jelas dalam setiap penerbitan atau penghasilan harta intelek menggunakan Geran ini. </p>
+			<p style="text-align: justify; padding-left: 120px"> ix.&Tab; Universiti berhak membatalkan kelulusan geran dan menamatkan projek sekiranya peruntukan tidak digunakan setelah enam (6) bulan daripada tarikh kelulusan dikeluarkan atau prestasi projek tidak memuaskan. </p>
+			<p style="text-align: justify; padding-left: 120px"> x.&Tab; Syarat-syarat tersebut di atas adalah tertakluk kepada pindaan Garis Panduan Pemantauan Geran Penyelidikan UiTM dari semasa ke semasa.  Penyelidik adalah diingatkan untuk sentiasa merujuk kepada Garis Panduan tersebut. </p>
+			<p style="text-align:justify;">3.&Tab;&Tab; Kelulusan ini <?php echo "$info"?>.</p>
+			<p>Sekian, terima kasih.</p>
+			<p><strong>“SELAMAT MENJALANKAN PENYELIDIKAN DENGAN JAYANYA”</strong></p>
+			<p>Yang benar</p>
+			<br>
+			<p><strong>PROFESOR DATO DR. JAMIL HAJI HAMALI<br>Rektor</strong></p>
+		</div>
+		<?php }?>
+	</body>
+</html>
+
+
+
+
+		
+
+	
