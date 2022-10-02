0d1c3c63b3677a8cd2cf16a8f69e3bbfa9f64be5
DataVerse98 <114841564+DataVerse98@users.noreply.github.com>
2022-10-02 12:46:29 +0800

    Delete php-html-js-css/eletter directory

--- php-html-js-css/eletter/ahli_projek4.php
+++
@@ -1,9 +0,0 @@
-<?php	
-	$query = "SELECT * FROM staff_info WHERE staff_id = $ap4";
-		
-	$query = $conn -> query($query);
-	$row = $query -> fetch_assoc();
-	
-	$nameap4 = $row['name'];
-	$cawanganap4 = $row['cawangan'];
-?>
